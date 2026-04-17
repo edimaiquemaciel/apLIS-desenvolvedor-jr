821c6fc1e25d3c1a645f33b1741f0cda44d5e118
Edimaique <edimaiqueacacio@gmail.com>
2026-04-17 17:41:52 -0300

    adiciona CORS no index.php

--- backendphp/public/index.php
+++ backendphp/public/index.php
@@ -2,6 +2,15 @@
 
 require_once __DIR__ . '/../vendor/autoload.php';
 
+header('Access-Control-Allow-Origin: *');
+header('Access-Control-Allow-Methods: GET, POST, PUT, DELETE, OPTIONS');
+header('Access-Control-Allow-Headers: Content-Type, Authorization');
+
+if ($_SERVER['REQUEST_METHOD'] === 'OPTIONS') {
+    http_response_code(200);
+    exit();
+}
+
 $lines = file(__DIR__ . '/../.env', FILE_IGNORE_NEW_LINES | FILE_SKIP_EMPTY_LINES);
 foreach ($lines as $line) {
     if (str_starts_with(trim($line), '#')) continue;
