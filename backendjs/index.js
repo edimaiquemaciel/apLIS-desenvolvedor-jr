91142d2c70e560af31c907694587e17d60f413ee
Edimaique <edimaiqueacacio@gmail.com>
2026-04-21 15:02:46 -0300

    ignorar arquivos .env e pasta vendor

--- backendjs/index.js
+++ backendjs/index.js
@@ -4,7 +4,6 @@ const app = express();
 
 app.use(express.json());
 
-// CORS
 app.use((req, res, next) => {
     res.header('Access-Control-Allow-Origin', '*');
     res.header('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE, OPTIONS');
